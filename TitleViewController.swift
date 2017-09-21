92289a24e3b1724622c62107ddb1fc9bdb3781b9
nttr <nttr@BI-23560.local>
2017-09-21 17:58:23 +0900

    タイトル画面の仮作成

---
+++ TitleViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TitleViewController.swift
+//  KoroKoroFurby
+//
+//  Created by nttr on 2017/09/21.
+//  Copyright © 2017年 nttr.hirtoki. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+    
+    @IBOutlet var startLabel: UILabel!;
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func start(){
+        self.performSegue(withIdentifier: "toGame", sender: nil);
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
